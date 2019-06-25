ef682ead0d9a74fefbadf65b41f27852cff03aee
FOSSE <raphael.fosse91@gmail.com>
2019-06-25 12:09:02 +0200

    article converter

--- videoClubMVC/src/main/java/converter/ArticleConverter.java
+++ videoClubMVC/src/main/java/converter/ArticleConverter.java
@@ -1,5 +1,28 @@
 package converter;
 
-public class ArticleConverter {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+
+import videoClub.model.Article;
+import videoClub.repository.ArticleRepository;
+
+public class ArticleConverter implements Converter<String, Article> {
+
+	@Autowired
+	private ArticleRepository articleRepository;
+
+	@Override
+	public Article convert(String source) {
+		if (!source.isEmpty()) {
+
+			Optional<Article> opt = articleRepository.findById(Integer.parseInt(source));
+			if (opt.isPresent()) {
+				return opt.get();
+			}
+		}
+		return null;
+	}
 
 }
