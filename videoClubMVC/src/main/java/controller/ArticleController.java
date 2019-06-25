5484580df1a6a1e28146f05565b163397f384395
FOSSE <raphael.fosse91@gmail.com>
2019-06-25 11:19:51 +0200

    controller_article_done

--- videoClubMVC/src/main/java/controller/ArticleController.java
+++ videoClubMVC/src/main/java/controller/ArticleController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import videoClub.model.Article;
+import videoClub.model.BluRay;
+import videoClub.model.Dvd;
 import videoClub.repository.ArticleRepository;
 
 
@@ -18,59 +21,59 @@ import videoClub.repository.ArticleRepository;
 public class ArticleController {
 
 	@Autowired
-	private ArticleRepository armeRepository;
+	private ArticleRepository articleRepository;
 
 	@GetMapping("/list")
 	public ModelAndView list() {
 		return new ModelAndView("article/list", "articles", articleRepository.findAll());
 	}
 
-	@GetMapping("/addEpee")
-	public ModelAndView addEpee() {
-		return goEdit(new Epee());
+	@GetMapping("/addDvd")
+	public ModelAndView addDvd() {
+		return goEdit(new Dvd());
 	}
 
-	@GetMapping("/addPistolet")
-	public ModelAndView addPistolet() {
-		return goEdit(new Pistolet());
+	@GetMapping("/addBlueray")
+	public ModelAndView addBlueray() {
+		return goEdit(new BluRay());
 	}
 
-	private ModelAndView goEdit(Arme arme) {
-		return new ModelAndView("arme/edit", "arme", arme);
+	private ModelAndView goEdit(Article article) {
+		return new ModelAndView("article/edit", "article", article);
 	}
 
-	@GetMapping("/saveEpee")
-	public ModelAndView saveEpee(@ModelAttribute("arme") Epee e) {
-		return save(e);
+	@GetMapping("/saveDvd")
+	public ModelAndView saveDvd(@ModelAttribute("article") Dvd d) {
+		return save(d);
 	}
 
-	@GetMapping("/savePistolet")
-	public ModelAndView savePistolet(@ModelAttribute("arme") Pistolet p) {
-		return save(p);
+	@GetMapping("/saveBlueray")
+	public ModelAndView saveBlueray(@ModelAttribute("article") BluRay b) {
+		return save(b);
 	}
 
-	private ModelAndView save(Arme arme) {
-		armeRepository.save(arme);
-		return new ModelAndView("redirect:/arme/list");
+	private ModelAndView save(Article article) {
+		articleRepository.save(article);
+		return new ModelAndView("redirect:/article/list");
 	}
 
 	@GetMapping("/edit")
 	public ModelAndView edit(@RequestParam(name = "id") Integer id) {
-		Optional<Arme> opt = armeRepository.findById(id);
+		Optional<Article> opt = articleRepository.findById(id);
 		if (opt.isPresent()) {
 			return goEdit(opt.get());
 		} else {
-			return new ModelAndView("redirect:/arme/list");
+			return new ModelAndView("redirect:/article/list");
 		}
 	}
 
 	@GetMapping("/delete")
 	public String delete(@RequestParam(name = "id") int id) {
-		Optional<Arme> opt = armeRepository.findById(id);
+		Optional<Article> opt = articleRepository.findById(id);
 		if (opt.isPresent()) {
-			armeRepository.deleteById(id);
+			articleRepository.deleteById(id);
 		}
-		return "redirect:/soldat/list";
+		return "redirect:/article/list";
 	}
 
 }
